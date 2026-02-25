f57e35976b3a65d5e43fa5254357efcc32a22629
Anugrahaa07 <anugraha.a2020@gmail.com>
2026-02-26 05:03:09 +0530

    LAB ASSIGNMENT CYCLE 1 QS 2

---
+++ c programs/LAB/qs2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int Largest(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b)
+    {
+        if (a > c)
+            largest = a;
+        else
+            largest = c;
+    }
+    else
+    {
+        if (b > c)
+            largest = b;
+        else
+            largest = c;
+    }
+
+    return largest;
+}
+
+int Smallest(int a, int b, int c)
+{
+    int smallest;
+
+    if (a < b && a < c)
+        smallest = a;
+    else if (b < c)
+        smallest = b;
+    else
+        smallest = c;
+
+    return smallest;
+}
+
+int Average(int a, int b, int c)
+{
+    float avg;
+    int A;
+
+    avg = (a + b + c) / 3.0;
+    A = (avg >= 50) ? 1 : 0;   // Conditional operator
+
+    printf("Average = %.2f\n", avg);
+    return A;
+}
+
+int main()
+{
+    int m1, m2, m3;
+    int largest, smallest, A;
+
+    printf("Enter marks of three subjects: ");
+    scanf("%d %d %d", &m1, &m2, &m3);
+
+    largest = Largest(m1, m2, m3);
+    smallest = Smallest(m1, m2, m3);
+    A = Average(m1, m2, m3);
+
+    printf("Largest mark = %d\n", largest);
+    printf("Smallest mark = %d\n", smallest);
+    printf("The average is = %d\n", A);
+
+    return 0;
+}
